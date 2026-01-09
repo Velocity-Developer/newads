e3d039bb104185304954bfe2407b2945d4746c36
adityathok <aditya0thok@gmail.com>
2026-01-09 10:03:11 +0700

    feat: Setting

---
+++ app/Models/Setting.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Setting extends Model
+{
+    protected $fillable = [
+        'key',
+        'value',
+    ];
+
+    /**
+     * Get a setting value by key
+     */
+    public static function get(string $key, mixed $default = null): mixed
+    {
+        $setting = static::where('key', $key)->first();
+
+        if (!$setting) {
+            return $default;
+        }
+
+        return $setting->value;
+    }
+
+    /**
+     * Set a setting value by key
+     */
+    public static function set(string $key, mixed $value): void
+    {
+        static::updateOrCreate(
+            ['key' => $key],
+            ['value' => $value]
+        );
+    }
+
+    /**
+     * Remove a setting by key
+     */
+    public static function remove(string $key): void
+    {
+        static::where('key', $key)->delete();
+    }
+
+    /**
+     * Check if a setting exists
+     */
+    public static function has(string $key): bool
+    {
+        return static::where('key', $key)->exists();
+    }
+}
